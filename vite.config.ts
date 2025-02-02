2845b3c05801241f96ad924ae65d5d449b007a2a
Memet Zx <zxce3@gnuweeb.org>
2025-02-02 12:44:44 +0700

    Update TypeScript configuration and Vite alias for worker paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'$workers': fileURLToPath(new URL('./src/lib/workers', import.meta.url))
+		}
+	}
 });
